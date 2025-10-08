8dd508d968392fe85522621317d25283c668c521
QPDoyle <qpdoyle13@gmail.com>
2025-10-08 12:38:38 -0400

    starting gameboard tests

---
+++ utils.py
@@ -0,0 +1,14 @@
+##
+
+# --- Window constants ---
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Tile Example"
+
+# --- Tile constants ---
+TILE_SCALE = 0.4
+TILE_WIDTH = 100 * TILE_SCALE
+TILE_HEIGHT = 150 * TILE_SCALE
+
+PEG_SPACING_X = TILE_WIDTH * 0.2   # 20% of tile width
+PEG_SPACING_Y = TILE_HEIGHT * 0.3  # 30% of tile height
