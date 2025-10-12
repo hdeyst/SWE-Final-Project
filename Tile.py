5ad2e1fc5106c1baf8907e0012a253b030654ee3
Bee Millie <bee.millie137@gmail.com>
2025-10-12 11:01:05 -0400

    Collection Class basic functionality

--- Tile.py
+++ Tile.py
@@ -2,3 +2,23 @@ class Tile:
     def __init__(self, num, col):
         self.number = num
         self.color = col
+
+class Collection:
+    def __init__(self, item): #assuming a collection will be created whenever a tile is added to the board alone
+        self.tiles = [item]
+
+    def add(self, item, index):
+        self.tiles.insert(index, item) #ensuring the list will be in the same order as on the board
+                                       #could use positions to determine order if we want to make Tile less abstract?
+
+    def remove(self, item):
+        self.tiles.remove(item)
+
+    def start(self):
+        return self.tiles[0]
+
+    def length(self):
+        return len(self.tiles)
+
+    def last(self):
+        return self.tiles[-1]
