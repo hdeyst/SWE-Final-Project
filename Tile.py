584edb1ca2a311ad8d5a2205612a7acdc28d3177
bobbypaletta <rpaletta@uvm.edu>
2025-10-15 12:39:01 -0400

    Merged quinns test.py and imlemented our tile and collection class

--- Tile.py
+++ Tile.py
@@ -1,7 +1,9 @@
+
 class Tile:
     def __init__(self, num, col):
         self.number = num
         self.color = col
+        self.image_file_name = f":tiles/{self.color}{self.number}.png"
 
 class Collection:
     def __init__(self, item): #assuming a collection will be created whenever a tile is added to the board alone
@@ -10,7 +12,6 @@ class Collection:
     def add(self, item, index):
         self.tiles.insert(index, item) #ensuring the list will be in the same order as on the board
                                        #could use positions to determine order if we want to make Tile less abstract?
-
     def remove(self, item):
         self.tiles.remove(item)
 
--- test.py
+++ test.py
@@ -11,6 +11,31 @@ class Tile(arcade.Sprite):
     def __init__(self, filename, scale=1):
         super().__init__(filename, scale)
 
+class Collection:
+    def __init__(self, item, isSet): #assuming a collection will be created whenever a tile is added to the board alone
+        self.tiles = [item]
+        self.isSet = isSet
+
+    def add(self, item, index):
+        self.tiles.insert(index, item) #ensuring the list will be in the same order as on the board
+                                       #could use positions to determine order if we want to make Tile less abstract?
+    def remove(self, item):
+        self.tiles.remove(item)
+
+    def getValue(self):
+        sum = 0
+        for e in self.tiles:
+            sum += e.number
+        return sum
+
+    def isSet(self):
+        isSet = True
+        for e in self.tiles:
+            if e.number != self.tiles[0]:
+                isSet = False
+        return isSet
+
+
 class Peg(arcade.Sprite):
     """A single peg sprite."""
     def __init__(self, filename, scale=1):
