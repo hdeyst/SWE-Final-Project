2749bff1541fc1f596a3837063d714b5d893f75b
hannah <hdeyst@uvm.edu>
2025-10-16 10:51:59 -0400

    tiles are drawn as a stack on the board. gotta merge the mouse stuff now (drag/drop)

--- combo_board.py
+++ combo_board.py
@@ -17,10 +17,17 @@ WINDOW_WIDTH = (TILE_WIDTH + INNER_MARGIN) * COLUMN_COUNT + OUTER_MARGIN * 2
 WINDOW_HEIGHT = (TILE_HEIGHT + INNER_MARGIN) * ROW_COUNT + OUTER_MARGIN * 2
 WINDOW_TITLE = "Rummikub Game Board!"
 
+class Tile(arcade.Sprite):
+    """A single tile sprite."""
+    def __init__(self, filename, scale=1):
+        super().__init__(filename, scale)
+
 class GameView(arcade.View):
     """
     Main application class.
     """
+    # TODO: convert this into a gameboard class
+    # TODO: snap tiles to peg center
 
     def __init__(self):
         """
@@ -32,16 +39,12 @@ class GameView(arcade.View):
         self.background_color = arcade.color.ASH_GREY
 
         # One dimensional list of all sprites in the two-dimensional sprite list
-        self.grid_sprite_list = arcade.SpriteList()
-
-        # This will be a two-dimensional grid of sprites to mirror the two
-        # dimensional grid of numbers. This points to the SAME sprites that are
-        # in grid_sprite_list, just in a 2d manner.
-        self.grid_sprites = []
+        self.peg_sprite_list = arcade.SpriteList()
+        self.peg_sprites = [] # 2D grid w/ same sprite objects as in peg_sprite_list
 
         # Create a list of solid-color sprites to represent each grid location
         for row in range(ROW_COUNT):
-            self.grid_sprites.append([])
+            self.peg_sprites.append([])
             for column in range(COLUMN_COUNT):
                 x = column * (TILE_WIDTH + INNER_MARGIN) + (TILE_WIDTH / 2 + INNER_MARGIN) + OUTER_MARGIN
                 y = row * (TILE_HEIGHT + INNER_MARGIN) + (TILE_HEIGHT / 2 + INNER_MARGIN) + OUTER_MARGIN
@@ -50,8 +53,37 @@ class GameView(arcade.View):
 
                 sprite.center_x = x
                 sprite.center_y = y
-                self.grid_sprite_list.append(sprite)
-                self.grid_sprites[row].append(sprite)
+                self.peg_sprite_list.append(sprite)
+                self.peg_sprites[row].append(sprite)
+
+        # create list of tile sprites (which will use quinn's tiles)
+        self.tile_list = arcade.SpriteList()
+
+        self.held_tiles = None
+        self.held_tiles_original_position = None
+
+
+    def setup(self):
+        """Set up the game here. Call this to restart."""
+        self.held_tiles = []
+        self.held_tiles_original_position = []
+
+        for i in range(4):
+            for j in range(13):
+                if i == 0:
+                    color = "cyan"
+                elif i == 1:
+                    color = "red"
+                elif i == 2:
+                    color = "yellow"
+                else:
+                    color = "black"
+                tile = Tile(f"tiles/{color}_{j + 1}.png", TILE_SCALE)
+
+                # Stacked tile placement, places all tiles in the top left stacked on one another
+                tile.center_x = 0 + TILE_WIDTH
+                tile.center_y = WINDOW_HEIGHT - TILE_HEIGHT
+                self.tile_list.append(tile)
 
 
     def on_draw(self):
@@ -62,9 +94,14 @@ class GameView(arcade.View):
         self.clear()
 
         # Batch draw the grid sprites
-        self.grid_sprite_list.draw()
-        for s in self.grid_sprite_list:
-            arcade.draw_point(s.center_x, s.center_y, arcade.color.BLACK, size=10)
+        self.peg_sprite_list.draw()
+
+        # draw the center points of each grid square
+        for s in self.peg_sprite_list:
+            arcade.draw_point(s.center_x, s.center_y, arcade.color.WHITE, size=5)
+
+        # draw the tiles
+        self.tile_list.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
         """
@@ -84,10 +121,10 @@ class GameView(arcade.View):
             return
 
         # Flip the color of the sprite
-        if self.grid_sprites[row][column].color == arcade.color.CEIL:
-            self.grid_sprites[row][column].color = arcade.color.LAVENDER_BLUE
+        if self.peg_sprites[row][column].color == arcade.color.CEIL:
+            self.peg_sprites[row][column].color = arcade.color.LAVENDER_BLUE
         else:
-            self.grid_sprites[row][column].color = arcade.color.CEIL
+            self.peg_sprites[row][column].color = arcade.color.CEIL
 
 
 def main():
@@ -97,6 +134,7 @@ def main():
 
     # Create the GameView
     game = GameView()
+    game.setup()
 
     # Show GameView on screen
     window.show_view(game)
