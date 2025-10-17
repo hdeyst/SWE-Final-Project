58158e87075667ba12a95feb1660a43f459bf587
Bee Millie <bee.millie137@gmail.com>
2025-10-17 11:48:07 -0400

    Updated Collection class in test.py

--- test.py
+++ test.py
@@ -20,28 +20,55 @@ class Tile(arcade.Sprite):
 
 
 class Collection:
-    def __init__(self, item, isSet): #assuming a collection will be created whenever a tile is added to the board alone
-        self.tiles = [item]
-        self.isSet = isSet
+    def __init__(self, tile): #assuming a collection will be created whenever a tile is added to the board alone
+        self.tiles = [tile]
+        self.isSet = True
 
-    def add(self, item, index):
-        self.tiles.insert(index, item) #ensuring the list will be in the same order as on the board
+    def add(self, tile, index):
+        self.tiles.insert(index, tile) #ensuring the list will be in the same order as on the board
                                        #could use positions to determine order if we want to make Tile less abstract?
+
     def remove(self, item):
         self.tiles.remove(item)
 
     def getValue(self):
         sum = 0
-        for e in self.tiles:
-            sum += e.number
+        for tile in self.tiles:
+            sum += tile.number
         return sum
 
-    def isSet(self):
-        isSet = True
-        for e in self.tiles:
-            if e.number != self.tiles[0]:
-                isSet = False
-        return isSet
+    def set(self): #3-4 same number, different colors
+        self.isSet = True
+        if len(self.tiles) == 3:
+            if self.tiles[0].color == self.tiles[1].color or self.tiles[0].color == self.tiles[2].color or self.tiles[1].color == self.tiles[2].color:
+                return False
+        elif len(self.tiles) == 4:
+            if self.tiles[0].color == self.tiles[1].color or self.tiles[0].color == self.tiles[2].color or self.tiles[0].color == self.tiles[3].color\
+                    or self.tiles[1].color == self.tiles[2].color or self.tiles[1].color == self.tiles[3].color or self.tiles[2].color == self.tiles[3].color:
+                return False
+        else: #incorrect length for set
+            return False
+        for tile in self.tiles:
+            if tile.number != self.tiles[0].number:
+                return False
+        return self.isSet
+
+    def run(self): #3+ same color, increasing numbers
+        if len(self.tiles) < 3:
+            return False
+        for index in range(len(self.tiles)):
+            if self.tiles[index].color != self.tiles[0].color:
+                return False
+            if index > 0 and not self.tiles[index - 1].number < self.tiles[index].number:
+                return False
+        return True
+
+
+
+    def isValid(self):
+        if self.set() == True or self.run() == True:
+            return True
+        return False
 
 
 class Peg(arcade.Sprite):
