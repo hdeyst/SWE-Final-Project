230c12bc34161f5d9f5b0124f8250f884d9eb648
Bee Millie <bee.millie137@gmail.com>
2025-11-17 04:11:21 -0500

    player can add tiles to hand and sort hand

--- ai_player.py
+++ ai_player.py
@@ -11,9 +11,62 @@ Functions:
 
 """
 import arcade
-from utils import COLUMN_COUNT_DOCK, ROW_COUNT_DOCK
+from tile import Tile
+from arcade import SpriteList
+from utils import TILE_SCALE
 
 
 class Player:
-    def __init__(self, user):
-        pass
+    def __init__(self):
+        self.hand = SpriteList()
+        self.is_turn = False
+        self.initial_melt = False
+
+    def deal(self, tile):
+        self.hand.append(tile)
+
+    def sort_sets(self):
+        self.hand.sort(key=lambda tile: tile.color)
+        self.hand.sort(key=lambda tile: tile.number)
+
+    def sort_runs(self):
+        self.hand.sort(key=lambda tile: tile.number)
+        self.hand.sort(key=lambda tile: tile.color)
+
+    def find_collection(self):
+        collections = []
+
+
+
+    def __repr__(self):
+        """representation of player's hand for testing"""
+        output = ""
+        for tile in self.hand[:-1]:
+            output += f"{tile}, "
+        output += f"{self.hand[-1]}, "
+        return output
+
+if __name__ == "__main__":
+    red2 = Tile(f"tiles/red_2.png", scale = TILE_SCALE)
+    red3 = Tile(f"tiles/red_3.png", scale = TILE_SCALE)
+    red4 = Tile(f"tiles/red_4.png", scale = TILE_SCALE)
+    yellow2 = Tile(f"tiles/yellow_2.png", scale = TILE_SCALE)
+    yellow3 = Tile(f"tiles/yellow_3.png", scale = TILE_SCALE)
+    black2 = Tile(f"tiles/black_2.png", scale = TILE_SCALE)
+    black5 = Tile(f"tiles/black_5.png", scale = TILE_SCALE)
+
+    player = Player()
+    player.deal(red2)
+    player.deal(red3)
+    player.deal(red4)
+    player.deal(yellow2)
+    player.deal(yellow3)
+    player.deal(black2)
+    player.deal(black5)
+    player.hand.shuffle()
+
+    print(player)
+    player.sort_sets()
+    print(f"set sorted: {player}")
+    player.sort_runs()
+    print(f"run sorted: {player}")
